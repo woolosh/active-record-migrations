0e84d9c9ac5da3907d9fac05bc44b88d878d4d83
woolosh <joshuapmorales@gmail.com>
2021-01-31 14:57:47 -0600

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,4 @@
+#config/environment.rb
 require 'rake'
 require 'active_record'
 require 'yaml/store'
@@ -8,8 +9,10 @@ require 'date'
 require 'bundler/setup'
 Bundler.require
 
-
 # put the code to connect to the database here
-
+ActiveRecord::Base.establish_connection(
+    :adapter => "sqlite3",
+    :database => "db/artists.sqlite"
+)
 
 require_relative "../artist.rb"
